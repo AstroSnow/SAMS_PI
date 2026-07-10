ef309a207245c0d7b061681da3b084f70ec4a0c7
snow <b.snow@exeter.ac.uk>
2026-07-10 08:57:53 +0100

    added minimum initial timestep

--- src/PhysicsPackages/TWOFLUID/twofluid.h
+++ src/PhysicsPackages/TWOFLUID/twofluid.h
@@ -70,7 +70,7 @@ namespace TWOFLUID
        
         //Coupling physics
         bool collisions=true;
-        bool ion_rec_empirical=false;
+        bool ion_rec_empirical=true;
         bool ion_rec_nlevel=false;
     };
  
@@ -128,6 +128,9 @@ namespace TWOFLUID
                 printf("two_fluid timestep = %f \n",plasma_source.two_fluid_timestep);
                 //set_dt(data);
                 timeData.dt = plasma_source.two_fluid_timestep<timeData.dt ? plasma_source.two_fluid_timestep : timeData.dt;
+                if (timeData.time<=1.0e-6) {
+                    timeData.dt = 1.0e-8<timeData.dt ? 1.0e-8 : timeData.dt;
+                }
             };
             void getTimestep(SAMS::timeState &timeData, LARE::LARE3DNF<T_EOS>::simulationData &dataNeutral){
                 //data.dt = timeData.dt;
